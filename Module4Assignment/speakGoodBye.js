c41b7d8ac06f640e64e8834945bad3b55d9ddbb4
Jubril Bolajoko Gbadebo <gbadebojubril@gmail.com>
2022-01-23 16:18:35 +0100

    Update speakGoodBye.js

--- Module4Assignment/speakGoodBye.js
+++ Module4Assignment/speakGoodBye.js
@@ -1,5 +1,4 @@
 (function(window){
-    let names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     let callJsomeNames = function (){
         for(let list in names){
             if(names[list].charAt(0) === "J" || names[list].charAt(0) === "j"){
